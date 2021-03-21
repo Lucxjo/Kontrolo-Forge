64cd364555b74670f63a257d73d6ceb2c32fad7c
Louis Hollingworth <louis@hollingworth.ch>
2021-03-21 17:05:37 +0000

    Added all weather stuff and refactored some code! Translation strings added! Almost ready for first release

--- src/main/kotlin/xyz/ludoviko/ktrl/ui/KScreen.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/ui/KScreen.kt
@@ -1,5 +1,7 @@
 package xyz.ludoviko.ktrl.ui
 
+import com.mojang.blaze3d.matrix.MatrixStack
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.widget.button.Button
 import net.minecraft.util.text.StringTextComponent
@@ -7,7 +9,7 @@ import net.minecraft.util.text.TranslationTextComponent
 import xyz.ludoviko.ktrl.ui.widget.KButton
 
 
-class KScreen : Screen(StringTextComponent("")) {
+class KScreen : Screen(StringTextComponent("Kontrolo")) {
     /** Width of a button  */
     private val BUTTON_WIDTH = 200
 
@@ -19,6 +21,9 @@ class KScreen : Screen(StringTextComponent("")) {
 
     override fun init() {
         super.init()
+
+        addWeather()
+
         addButton(KButton(
             (width - BUTTON_WIDTH) / 2,
             height - DONE_BUTTON_TOP_OFFSET,
@@ -26,5 +31,34 @@ class KScreen : Screen(StringTextComponent("")) {
             TranslationTextComponent("gui.done")
         )  // Action performed when the button is pressed
         { button: Button? -> onClose() })
+    }
+
+    private fun addWeather() {
+        addButton(KButton(
+            20, 55, 120, 20, TranslationTextComponent("text.ktrl.weather.clear.label")
+        ) {
+            Minecraft.getInstance().player?.chat("/weather clear 15000")
+        })
+        addButton(KButton(
+            150, 55, 120, 20, TranslationTextComponent("text.ktrl.weather.rain.label")
+        ) {
+            Minecraft.getInstance().player?.chat("/weather rain 15000")
+        })
+        addButton(KButton(
+            280, 55, 120, 20, TranslationTextComponent("text.ktrl.weather.thunder.label")
+        ) {
+            Minecraft.getInstance().player?.chat("/weather thunder 15000")
+        })
+    }
+
+    override fun render(matrixStack: MatrixStack, mouseX: Int, mouseY: Int, partialTicks: Float) {
+        this.renderBackground(matrixStack)
+        drawCenteredString(matrixStack, this.font, this.title.string, this.width / 2, 20, 0xFFFFFF)
+        drawString(
+            matrixStack, this.font,
+            TranslationTextComponent("text.ktrl.weather.label"),
+            20, 40, 0xFFFFFF
+        )
+        super.render(matrixStack, mouseX, mouseY, partialTicks)
     }
 }
