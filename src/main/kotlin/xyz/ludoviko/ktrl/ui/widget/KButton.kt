990be300c19ab602bd40329b37228ca4ac7ec370
Louis Hollingworth <louis@hollingworth.ch>
2021-03-21 15:27:18 +0000

    Updated KButton to allow for tooltips!

--- src/main/kotlin/xyz/ludoviko/ktrl/ui/widget/KButton.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/ui/widget/KButton.kt
@@ -4,5 +4,11 @@ import net.minecraft.client.gui.widget.button.Button
 import net.minecraft.util.text.ITextComponent
 
 class KButton(
-    xPos: Int, yPos: Int, width: Int, height: Int, text: ITextComponent, action: Button.IPressable
-) : Button(xPos, yPos, width, height, text, action)
+    xPos: Int,
+    yPos: Int,
+    width: Int,
+    height: Int,
+    text: ITextComponent,
+    tooltip: Button.ITooltip = NO_TOOLTIP,
+    action: Button.IPressable
+) : Button(xPos, yPos, width, height, text, action, tooltip)
