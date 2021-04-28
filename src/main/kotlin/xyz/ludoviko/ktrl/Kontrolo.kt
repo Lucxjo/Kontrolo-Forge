dd3129b2b08ba255aaaf0183a13932406e5f592d
Louis Hollingworth <louis@hollingworth.ch>
2021-04-28 21:27:43 +0100

    Commented out Config GUI Factory for patch release.

--- src/main/kotlin/xyz/ludoviko/ktrl/Kontrolo.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/Kontrolo.kt
@@ -1,10 +1,6 @@
 package xyz.ludoviko.ktrl
 
-import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.screen.Screen
 import net.minecraftforge.common.ForgeConfigSpec
-import net.minecraftforge.fml.ExtensionPoint
-import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
@@ -13,9 +9,7 @@ import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.registerConfig
 import xyz.ludoviko.ktrl.key.Key
-import xyz.ludoviko.ktrl.ui.ConfigGUI
 import xyz.ludoviko.ktrl.util.conf.Holder
-import java.util.function.BiFunction
 
 
 @Mod(Kontrolo.ID)
@@ -31,9 +25,9 @@ object Kontrolo {
         MOD_BUS.addListener(Holder::sync)
         FORGE_BUS.addListener(Key::onEvent)
 
-        ModLoadingContext.get().registerExtensionPoint(
+        /*ModLoadingContext.get().registerExtensionPoint(
             ExtensionPoint.CONFIGGUIFACTORY
-        ) { BiFunction { mc: Minecraft?, screen: Screen? -> ConfigGUI() } }
+        ) { BiFunction { mc: Minecraft?, screen: Screen? -> ConfigGUI() } }*/
 
         val specPair = ForgeConfigSpec.Builder().configure(Holder::configure)
         registerConfig(ModConfig.Type.CLIENT, specPair.right, "kontrolo.toml")
