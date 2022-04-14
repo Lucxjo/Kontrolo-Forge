9f6962d1cec04cb29f69162afd45d5b20193a350
Ludoviko <me@ludoviko.ch>
2022-04-14 19:18:04 +0100

    Removed KButton.kt and ConfigGUI.kt Updated AKScreen.kt, still needs further changing to work
    Signed-off-by: Ludoviko <me@ludoviko.ch>

--- src/main/kotlin/xyz/ludoviko/ktrl/ui/AKScreen.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/ui/AKScreen.kt
@@ -1,36 +1,38 @@
 package xyz.ludoviko.ktrl.ui
 
+import com.google.common.collect.Lists
 import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.components.Button
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.TextComponent
 import net.minecraft.network.chat.TranslatableComponent
-import xyz.ludoviko.ktrl.ui.widget.KButton
 import xyz.ludoviko.ktrl.util.cmd.Command
 
 abstract class AKScreen(title: Component) : Screen(title) {
+    private var buttons: MutableList<Button> = Lists.newArrayList()
     fun addKButton(xPos: Int, yPos: Int, width: Int, height: Int, enum: Command, translatable: Boolean = true) {
-        addButton(
-            KButton(
-                xPos, yPos, width, height, if (translatable) {
-                    TranslatableComponent(enum.text())
-                } else {
-                    TextComponent(enum.text())
-                }
-            ) {
-                Minecraft.getInstance().player?.chat(enum.command())
-                onClose()
-        })
+        this.buttons.add(
+            this.addRenderableWidget(
+                Button(
+                    xPos,
+                    yPos,
+                    width,
+                    height,
+                    if (translatable) TranslatableComponent(enum.text()) else TextComponent(enum.text())
+                ) {
+                    Minecraft.getInstance().player?.chat(enum.command())
+                    onClose()
+                })
+        )
     }
 
     fun addKButton(xPos: Int, yPos: Int, width: Int, height: Int, title: String, cmd: String) {
-        addButton(KButton(
-            xPos, yPos, width, height, TextComponent(title)
-        ) {
+        this.buttons.add(this.addRenderableWidget(Button(xPos, yPos, width, height, TextComponent(title)) {
             Minecraft.getInstance().player?.chat(cmd)
             onClose()
-        })
+        }))
     }
 
     // Helper function to ensure correct namings are used in function parameters
